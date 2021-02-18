55af926346f0ef9dbc747a5ab472a8fd47db241c
pedro.mda@gmail.com <Molly/2020>
2021-02-18 19:51:08 +0000

    draft do esqueleto do programa

---
+++ hospital-app/src/main/java/pt/iscte/hospital/LoginController.java
@@ -0,0 +1,17 @@
+package pt.iscte.hospital;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class LoginController {
+
+
+    @GetMapping(value = {"/", "/login"})
+    public String showLoginPage(){
+        return "login";
+    }
+
+}
