fa86d92b1cc000009974b36b48ec112d6f9f1c64
Ocram82 <ocram_manuel@hotmail.com>
2021-02-22 12:04:38 +0000

    #40 pagina user a receber informação da base de dados

--- hospital-app/src/main/java/pt/iscte/hospital/controllers/LoginController.java
+++ hospital-app/src/main/java/pt/iscte/hospital/controllers/LoginController.java
@@ -6,13 +6,17 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pt.iscte.hospital.entities.LogIn;
 import pt.iscte.hospital.services.LoginService;
+import pt.iscte.hospital.repositories.UserRepository;
 
 @Controller
 public class LoginController {
 
     @Autowired
     private LoginService loginService;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping(value = "/login")
     public String showLoginPage(){
@@ -23,6 +27,7 @@ public class LoginController {
     public String validateLogin(ModelMap map, @RequestParam String user_email, @RequestParam String password) {
         if (loginService.validateLogin(user_email, password)) {
             //true
+            LogIn.setConnectedUser(userRepository.findByEmail(user_email));
             return "redirect:/main";
         } else {
             //Invalid Credentials
