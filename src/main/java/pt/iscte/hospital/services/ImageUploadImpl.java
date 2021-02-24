def2e82f1690370dbd016eda5f5440e270f9032d
pedro.mda@gmail.com <Molly/2020>
2021-02-24 12:33:58 +0000

    # 46 funcionalidade de salvar imagens terminada

--- src/main/java/pt/iscte/hospital/services/ImageUploadImpl.java
+++ src/main/java/pt/iscte/hospital/services/ImageUploadImpl.java
@@ -2,30 +2,64 @@ package pt.iscte.hospital.services;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import pt.iscte.hospital.exceptions.ImageSizeException;
+import pt.iscte.hospital.exceptions.ImageTypeException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 
 @Service
 public class ImageUploadImpl implements ImageUploadService {
     private static final String dirSavePath = "uploaded_images/";
+    private static final long IMAGE_MAX_SIZE = 1000000L;   // Size in bytes
+    private static final String[] IMAGE_TYPES = {"image/jpeg", "image/png"};
 
+    // Methods
     @Override
-    public void uploadImage(MultipartFile imageFile, String username) throws IOException {
-        // TODO check if image jpg
-        // TODO check if size too large
-        // TODO permitir guardar por cima se já existir uma imagem
-        String rootDir = System.getProperty("user.dir"); // root directory of the project
-        String savePath = rootDir + "/" + dirSavePath;
-        //TODO
+    public void uploadImage(MultipartFile imageFile, String username)
+            throws IOException, ImageSizeException, ImageTypeException {
 
-        imageFile.transferTo(new File(savePath + username + ".jpg"));
+        // Check if size too big & Check if image jpg or png
+        verifyImage(imageFile);                              // Throws an error if not a valid image
+
+
+        // Sets save path and file extension
+        String contentType = imageFile.getContentType();
+        String rootDir = System.getProperty("user.dir");     // root directory of the project
+        String savePath = rootDir + "/" + dirSavePath;       // save directory path for images
+        String fileExtension = getFileExtension(contentType);
+
+        // Save image
+        imageFile.transferTo(new File(savePath + username + fileExtension));
     }
 
-    @Override
-    public boolean isImage(String fileType) {
-        //TODO
-        return true;
+    private void verifyImage(MultipartFile imageFile) throws ImageSizeException, ImageTypeException {
+        String contentType = imageFile.getContentType();
+        boolean isImage = false;
+
+        // Check if size too big
+        if (imageFile.getSize() > IMAGE_MAX_SIZE) {
+            throw new ImageSizeException();
+        }
+
+        // Check if image jpg or png
+        for (String imageType : IMAGE_TYPES) {
+            if (contentType.equals(imageType)) {
+                isImage = true;
+                break;
+            }
+        }
+        if (!isImage){
+            throw new ImageTypeException();
+        }
+    }
+
+    private String getFileExtension(String imageType){
+        HashMap<String, String> imageExtensions = new HashMap<>();
+        imageExtensions.put("image/jpeg",".jpg");
+        imageExtensions.put("image/png",".png");
+        return imageExtensions.get(imageType);
     }
 
 }
--- src/main/java/pt/iscte/hospital/services/ImageUploadService.java
+++ src/main/java/pt/iscte/hospital/services/ImageUploadService.java
@@ -1,10 +1,12 @@
 package pt.iscte.hospital.services;
 
 import org.springframework.web.multipart.MultipartFile;
+import pt.iscte.hospital.exceptions.ImageSizeException;
+import pt.iscte.hospital.exceptions.ImageTypeException;
 
 import java.io.IOException;
 
 public interface ImageUploadService {
-    void uploadImage(MultipartFile imageFile, String username) throws IOException;
-    boolean isImage(String fileType);
+    void uploadImage(MultipartFile imageFile, String username)
+            throws IOException, ImageSizeException, ImageTypeException;
 }
