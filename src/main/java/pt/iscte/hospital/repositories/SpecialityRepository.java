c5f822f6143a454480e85231ea3e53e1ea2f087b
pedro.mda@gmail.com <Molly/2020>
2021-02-26 12:38:02 +0000

    e #61 Criação das classes com ligação à base de dados (actualização)

--- src/main/java/pt/iscte/hospital/repositories/SpecialityRepository.java
+++ src/main/java/pt/iscte/hospital/repositories/SpecialityRepository.java
@@ -2,7 +2,8 @@ package pt.iscte.hospital.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pt.iscte.hospital.entities.Speciality;
 
 @Repository
-public interface SpecialityRepository  extends JpaRepository<SpecialityRepository,Long> {
+public interface SpecialityRepository  extends JpaRepository<Speciality,Long> {
 }
