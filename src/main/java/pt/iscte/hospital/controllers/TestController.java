ef8df00327fdbc3e86c5ca452f72aa71a10db7d5
carlan92 <68751305+carlan92@users.noreply.github.com>
2021-03-04 03:16:54 +0000

    #81 Adaptação páginas Consultas para cada tipo de user

--- src/main/java/pt/iscte/hospital/controllers/TestController.java
+++ src/main/java/pt/iscte/hospital/controllers/TestController.java
@@ -47,30 +47,29 @@ public class TestController {
         return "historicoPagamentos";
     }
 
-    @GetMapping(value = "/lista-utentes")
-    public String showListaUtentes(ModelMap modelMap) {
-        List<Speciality> specialities = specialityService.findAll(Sort.by(Sort.Direction.ASC, "name"));
+    @GetMapping(value = "/lista_utentes_recep")
+    public String showListaUtentestoRecep(ModelMap modelMap) {
         List<Patient> patients = patientService.findAll(Sort.by(Sort.Direction.ASC, "name"));
         User userLogged = Login.getConnectedUser();
 
-        modelMap.put("specialities", specialities);
         modelMap.put("patients", patients);
         modelMap.put("user_logged", userLogged);
-        return "lista-utentes";
+        return "lista_utentes_recep";
     }
 
-    @GetMapping(value = "/doctor-consultas")
-    public String showDoctorConsultas(ModelMap modelMap) {
-        List<Speciality> specialities = specialityService.findAll(Sort.by(Sort.Direction.ASC, "name"));
+    @GetMapping(value = "/lista_utentes_doctor")
+    public String showListaUtentestoDoctor(ModelMap modelMap) {
+        List<Patient> patients = patientService.findAll(Sort.by(Sort.Direction.ASC, "name"));
         User userLogged = Login.getConnectedUser();
 
-        modelMap.put("specialities", specialities);
+        modelMap.put("patients", patients);
         modelMap.put("user_logged", userLogged);
-        return "doctor-consultas";
+        return "lista_utentes_doctor";
     }
 
-    @GetMapping(value = "/lista-medicos")
-    public String showListaMedicos(ModelMap modelMap) {
+
+    @GetMapping(value = "/lista_medicos_recep")
+    public String showListaMedicostoRecep(ModelMap modelMap) {
         List<Speciality> specialities = specialityService.findAll(Sort.by(Sort.Direction.ASC, "name"));
         List<Doctor> doctors = doctorService.findAll(Sort.by(Sort.Direction.ASC, "name"));
         User userLogged = Login.getConnectedUser();
@@ -78,8 +77,67 @@ public class TestController {
         modelMap.put("specialities", specialities);
         modelMap.put("doctors", doctors);
         modelMap.put("user_logged", userLogged);
-        return "lista-medicos";
+        return "lista_medicos_recep";
     }
+
+    @GetMapping(value = "/lista_medicos_doctor_patient")
+    public String showListaMedicostoDoctorandPatient(ModelMap modelMap) {
+        List<Speciality> specialities = specialityService.findAll(Sort.by(Sort.Direction.ASC, "name"));
+        List<Doctor> doctors = doctorService.findAll(Sort.by(Sort.Direction.ASC, "name"));
+        User userLogged = Login.getConnectedUser();
+
+        modelMap.put("specialities", specialities);
+        modelMap.put("doctors", doctors);
+        modelMap.put("user_logged", userLogged);
+        return "lista_medicos_doctor_patient";
+    }
+
+    @GetMapping(value = "/consultas_doctor")
+    public String showConsultastoDoctor(ModelMap modelMap) {
+        List<Speciality> specialities = specialityService.findAll(Sort.by(Sort.Direction.ASC, "name"));
+        User userLogged = Login.getConnectedUser();
+
+        modelMap.put("specialities", specialities);
+        modelMap.put("user_logged", userLogged);
+        return "consultas_doctor";
+    }
+
+    @GetMapping(value = "/consultas_patient")
+    public String showConsultastoPatient(ModelMap modelMap) {
+        List<Speciality> specialities = specialityService.findAll(Sort.by(Sort.Direction.ASC, "name"));
+        User userLogged = Login.getConnectedUser();
+
+        modelMap.put("specialities", specialities);
+        modelMap.put("user_logged", userLogged);
+        return "consultas_patient";
+    }
+
+    @GetMapping(value = "/consultas_recep")
+    public String showConsultastoRecep(ModelMap modelMap) {
+        List<Speciality> specialities = specialityService.findAll(Sort.by(Sort.Direction.ASC, "name"));
+        User userLogged = Login.getConnectedUser();
+
+        modelMap.put("specialities", specialities);
+        modelMap.put("user_logged", userLogged);
+        return "consultas_recep";
+    }
+
+    @GetMapping(value = "/lista_espera_doctor")
+    public String showWaitListtoDoctor(ModelMap modelMap) {
+        User userLogged = Login.getConnectedUser();
+
+        modelMap.put("user_logged", userLogged);
+        return "lista_espera_doctor";
+    }
+
+    @GetMapping(value = "/lista_espera_recep")
+    public String showWaitListtoRecep(ModelMap modelMap) {
+        User userLogged = Login.getConnectedUser();
+
+        modelMap.put("user_logged", userLogged);
+        return "lista_espera_recep";
+    }
+
 
     @GetMapping(value = "/info-appointment")
     public String showInfoConsulta(ModelMap modelMap) {
