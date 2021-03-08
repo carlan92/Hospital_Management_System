19488ecf2ad6f7f4a678a5a21697dcc0128d21ee
Ocram82 <ocram_manuel@hotmail.com>
2021-03-08 22:57:44 +0000

    #94 alterar dados de medico e rececionista já actualizam na base de dados sem criar novas entradas

--- src/main/java/pt/iscte/hospital/controllers/UserController.java
+++ src/main/java/pt/iscte/hospital/controllers/UserController.java
@@ -37,8 +37,11 @@ public class UserController {
     @GetMapping(value = "/user/change-profile-data")
     public String showChangeProfileData(ModelMap modelMap) {
         List<Nationality> nationalities = nationalityService.findAll();
+        List<Speciality> specialities=specialityService.findAll(Sort.by(Sort.Direction.ASC,"name"));
 
         modelMap.put("nationalities", nationalities);
+        modelMap.put("specialities",specialities);
+
         modelMap.put("user_logged", userService.currentUser());
         modelMap.put("user", userService.currentUser());
 
@@ -46,72 +49,199 @@ public class UserController {
     }
 
     @PostMapping(value = "/user/change-profile-data")
-    public String returnToUserPage(@ModelAttribute Patient user,
+    public String changeDataPage(@ModelAttribute Patient patient,
+                                   @ModelAttribute Doctor doctor,
+                                   @ModelAttribute Receptionist receptionist,
                                    ModelMap modelMap,
                                    @RequestParam("file") MultipartFile file) {
 
         // Update user info
         User connectedUser = userService.currentUser();
+        if (userService.currentUser().getAccount().equals("Utente")) {
+            userValidationService.clear().setUser(patient)
+                    .validName()
+                    .validPhone()
+                    .validPostCode()
+                    .validSex()
+                    .validDocumentType()
+                    .validDocumentNumber()
+                    .validPatientNumber()
+                    .validNif()
+                    .validCity()
+                    .validBirthday()
+                    .validNationality()
+                    .validAddress();
 
-        userValidationService.clear().setUser(user)
-                .validName()
-                .validPhone()
-                .validPostCode()
-                .validSex()
-                .validDocumentType()
-                .validDocumentNumber()
-                .validPatientNumber()
-                .validNif()
-                .validCity()
-                .validBirthday()
-                .validNationality()
-                .validAddress();
-
-        if (!user.getDocumentNumber().equals(connectedUser.getDocumentNumber())) {
-            userValidationService.validDocumentNumberUnique();
-        }
-        if (user.getPatientNumber()!=null) {
-            if (!user.getPatientNumber().equals(connectedUser.getPatientNumber())) {
-                userValidationService.validPatientNumberUnique();
+            if (!patient.getDocumentNumber().equals(connectedUser.getDocumentNumber())) {
+                userValidationService.validDocumentNumberUnique();
             }
-        }
-
-        if (!user.getNif().equals(connectedUser.getNif())) {
-            userValidationService.validNifUnique();
-        }
-
-        if (file != null && !file.isEmpty() && !file.getContentType().equals("application/octet-stream")) {
-            try {
-                String photoURL = imageUploadService.uploadImage(file, userService.currentUser().getUsername());
-                user.setPhotoURL(photoURL);
-            } catch (IOException e) {
-                userValidationService.notValidPhotoUpload();
-            } catch (ImageTypeException e) {
-                userValidationService.notValidImageType();
-            } catch (ImageSizeException e) {
-                userValidationService.notValidImageSize();
+            if (patient.getPatientNumber() != null) {
+                if (!patient.getPatientNumber().equals(connectedUser.getPatientNumber())) {
+                    userValidationService.validPatientNumberUnique();
+                }
             }
-        } else {
-            user.setPhotoURL(userService.currentUser().getPhotoURL());
+
+            if (!patient.getNif().equals(connectedUser.getNif())) {
+                userValidationService.validNifUnique();
+            }
+
+            if (file != null && !file.isEmpty() && !file.getContentType().equals("application/octet-stream")) {
+                try {
+                    String photoURL = imageUploadService.uploadImage(file, userService.currentUser().getUsername());
+                    patient.setPhotoURL(photoURL);
+                } catch (IOException e) {
+                    userValidationService.notValidPhotoUpload();
+                } catch (ImageTypeException e) {
+                    userValidationService.notValidImageType();
+                } catch (ImageSizeException e) {
+                    userValidationService.notValidImageSize();
+                }
+            } else {
+                patient.setPhotoURL(userService.currentUser().getPhotoURL());
+            }
+
+            if (!userValidationService.isValid()) {
+                // case error in info validation
+                List<Nationality> nationalities = nationalityService.findAll();
+                modelMap.addAllAttributes(userValidationService.getErrorModelMap());
+                modelMap.put("nationalities", nationalities);
+
+                modelMap.put("user_logged", userService.currentUser());
+                modelMap.put("user", patient);
+                return "user/change-profile-data";
+            }
+            patient.setUserId(connectedUser.getUserId());
+            patient.setEmail(connectedUser.getEmail());
+            patient.setUsername(connectedUser.getUsername());
+            patient.setPassword(connectedUser.getPassword());
+            patient.setAccount(connectedUser.getAccount());
+
+            userService.addUser(patient);
+        } else if (userService.currentUser().getAccount().equals("Médico")) {
+            userValidationService.clear().setUser(doctor)
+                    .validName()
+                    .validPhone()
+                    .validPostCode()
+                    .validSex()
+                    .validDocumentType()
+                    .validDocumentNumber()
+                    .validPatientNumber()
+                    .validNif()
+                    .validCity()
+                    .validBirthday()
+                    .validNationality()
+                    .validAddress();
+
+            if (!doctor.getDocumentNumber().equals(connectedUser.getDocumentNumber())) {
+                userValidationService.validDocumentNumberUnique();
+            }
+            if (doctor.getPatientNumber() != null) {
+                if (!doctor.getPatientNumber().equals(connectedUser.getPatientNumber())) {
+                    userValidationService.validPatientNumberUnique();
+                }
+            }
+
+            if (!doctor.getNif().equals(connectedUser.getNif())) {
+                userValidationService.validNifUnique();
+            }
+
+            if (file != null && !file.isEmpty() && !file.getContentType().equals("application/octet-stream")) {
+                try {
+                    String photoURL = imageUploadService.uploadImage(file, userService.currentUser().getUsername());
+                    doctor.setPhotoURL(photoURL);
+                } catch (IOException e) {
+                    userValidationService.notValidPhotoUpload();
+                } catch (ImageTypeException e) {
+                    userValidationService.notValidImageType();
+                } catch (ImageSizeException e) {
+                    userValidationService.notValidImageSize();
+                }
+            } else {
+                doctor.setPhotoURL(userService.currentUser().getPhotoURL());
+            }
+
+            if (!userValidationService.isValid()) {
+                // case error in info validation
+                List<Nationality> nationalities = nationalityService.findAll();
+                modelMap.addAllAttributes(userValidationService.getErrorModelMap());
+                modelMap.put("nationalities", nationalities);
+
+                modelMap.put("user_logged", userService.currentUser());
+                modelMap.put("user", doctor);
+                return "user/change-profile-data";
+            }
+            doctor.setUserId(connectedUser.getUserId());
+            doctor.setEmail(connectedUser.getEmail());
+            doctor.setUsername(connectedUser.getUsername());
+            doctor.setPassword(connectedUser.getPassword());
+            doctor.setAccount(connectedUser.getAccount());
+            Speciality speciality = specialityService.findByName(((Doctor)connectedUser).getSpeciality().getName());
+            doctor.setSpeciality(speciality);
+
+            userService.addUser(doctor);
+
+        } else if (userService.currentUser().getAccount().equals("Recepcionista")) {
+            userValidationService.clear().setUser(receptionist)
+                    .validName()
+                    .validPhone()
+                    .validPostCode()
+                    .validSex()
+                    .validDocumentType()
+                    .validDocumentNumber()
+                    .validPatientNumber()
+                    .validNif()
+                    .validCity()
+                    .validBirthday()
+                    .validNationality()
+                    .validAddress();
+
+            if (!receptionist.getDocumentNumber().equals(connectedUser.getDocumentNumber())) {
+                userValidationService.validDocumentNumberUnique();
+            }
+            if (receptionist.getPatientNumber() != null) {
+                if (!receptionist.getPatientNumber().equals(connectedUser.getPatientNumber())) {
+                    userValidationService.validPatientNumberUnique();
+                }
+            }
+
+            if (!receptionist.getNif().equals(connectedUser.getNif())) {
+                userValidationService.validNifUnique();
+            }
+
+            if (file != null && !file.isEmpty() && !file.getContentType().equals("application/octet-stream")) {
+                try {
+                    String photoURL = imageUploadService.uploadImage(file, userService.currentUser().getUsername());
+                    receptionist.setPhotoURL(photoURL);
+                } catch (IOException e) {
+                    userValidationService.notValidPhotoUpload();
+                } catch (ImageTypeException e) {
+                    userValidationService.notValidImageType();
+                } catch (ImageSizeException e) {
+                    userValidationService.notValidImageSize();
+                }
+            } else {
+                receptionist.setPhotoURL(userService.currentUser().getPhotoURL());
+            }
+
+            if (!userValidationService.isValid()) {
+                // case error in info validation
+                List<Nationality> nationalities = nationalityService.findAll();
+                modelMap.addAllAttributes(userValidationService.getErrorModelMap());
+                modelMap.put("nationalities", nationalities);
+
+                modelMap.put("user_logged", userService.currentUser());
+                modelMap.put("user", receptionist);
+                return "user/change-profile-data";
+            }
+            receptionist.setUserId(connectedUser.getUserId());
+            receptionist.setEmail(connectedUser.getEmail());
+            receptionist.setUsername(connectedUser.getUsername());
+            receptionist.setPassword(connectedUser.getPassword());
+            receptionist.setAccount(connectedUser.getAccount());
+
+            userService.addUser(receptionist);
         }
 
-        if (!userValidationService.isValid()) {
-            // case error in info validation
-            List<Nationality> nationalities = nationalityService.findAll();
-            modelMap.addAllAttributes(userValidationService.getErrorModelMap());
-            modelMap.put("nationalities", nationalities);
-
-            modelMap.put("user_logged", userService.currentUser());
-            modelMap.put("user", user);
-            return "user/change-profile-data";
-        }
-        user.setUserId(connectedUser.getUserId());
-        user.setEmail(connectedUser.getEmail());
-        user.setUsername(connectedUser.getUsername());
-        user.setPassword(connectedUser.getPassword());
-        user.setAccount(connectedUser.getAccount());
-
-        userService.addUser(user);
 
         return "redirect:/user/user-profile";
     }
