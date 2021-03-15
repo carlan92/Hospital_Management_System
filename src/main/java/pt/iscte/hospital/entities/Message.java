c346b2699ed859ab5d0b339d34983d5cb6510dc1
pedro.mda@gmail.com <Molly/2020>
2021-03-15 20:37:34 +0000

    sistema de mensagens, criação do objecto.

---
+++ src/main/java/pt/iscte/hospital/entities/Message.java
@@ -0,0 +1,41 @@
+package pt.iscte.hospital.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Message {
+    // Attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long messageId;
+    private String subject;
+    private String message;
+    private LocalDateTime dateTime;
+    private boolean readMsg;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    // Constructor
+    public Message() {
+        readMsg = false;
+    }
+
+    public Message(String subject, String message, User userReceiver) {
+        this.subject = subject;
+        this.message = message;
+        this.dateTime = LocalDateTime.now();
+        this.user = userReceiver;
+        readMsg = false;
+    }
+
+    // Methods
+}
