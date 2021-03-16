3ec813add3b80f7b8263c13607313205f2407679
pedro.mda@gmail.com <Molly/2020>
2021-03-16 21:59:03 +0000

    sistema de mensagens

---
+++ src/main/java/pt/iscte/hospital/utils/Common.java
@@ -0,0 +1,37 @@
+package pt.iscte.hospital.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.ModelMap;
+import pt.iscte.hospital.entities.User;
+import pt.iscte.hospital.services.MessageService;
+import pt.iscte.hospital.services.user.UserService;
+
+@Component
+public class Common {
+    private final UserService userService;
+    private final MessageService messageService;
+
+    @Autowired
+    public Common(UserService userService, MessageService messageService) {
+        this.userService = userService;
+        this.messageService = messageService;
+    }
+
+
+
+    public User currentUser() {
+        return userService.currentUser();
+    }
+
+    public ModelMap sideNavMap() {
+        ModelMap modelMap = new ModelMap();
+
+        boolean hasUnreadMessages = messageService.hasUnreadMessages(currentUser().getUserId());
+
+        modelMap.put("user_logged", currentUser());
+        modelMap.put("hasUnreadMessages", hasUnreadMessages);
+
+        return modelMap;
+    }
+}
